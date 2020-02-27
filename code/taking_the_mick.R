81e121a9872737c77884398541832eef13fed372
ShaneODub <ShaneONeillja@gmail.com>
2020-02-27 18:15:45 +0000

    Fiddling with html_text html_nodes

--- code/taking_the_mick.R
+++ code/taking_the_mick.R
@@ -26,26 +26,34 @@ url <-
 list_of_pages <- vector("list", 72)
 
 # Now loop through the 26th to 33rd Dail, grabbing and storing 9 pages from each.
-for (term in 26:33) {
-  for (page in 1:9){
-    list_of_pages[[page + 9 * (term - 26)]] <-
-      paste0(url, term, '&tab=constituency&page=', page) %>%
-      read_html()
-    Sys.sleep(3)
-    print(page + 9 * (term - 26))
-  }
-}
+# for (term in 26:33) {
+#   for (page in 1:9){
+#     list_of_pages[[page + 9 * (term - 26)]] <-
+#       paste0(url, term, '&tab=constituency&page=', page) %>%
+#       read_html()
+#     Sys.sleep(3)
+#     print(page + 9 * (term - 26))
+#   }
+# }
 
-extracted_data <- tibble(term = integer(1500),
-                         member = character(1500),
-                         constituency = character(1500),
-                         party = character(1500))
+# Output the list so I won't have to scrape again
+saveRDS(list_of_pages, file = "data//list_of_pages.rds")
+list_of_pages <- readRDS("data//list_of_pages.rds")
+
+# Make a 1500-length tibble because 8 terms X 9 pages X 20 TDs = 1440
+extracted_data <- tibble(term = integer(1440),
+                         member = character(1440),
+                         constituency = character(1440),
+                         party = character(1440),
+                         profile_url = character(1440))
 
 for (term in 26:33) {
-  for (page in 1:9){
+  for (page in 1:9) {
+    max_td <- 
     for (td in 1:20) {
-    extracted_data$term[[20 * (page - 1) + 9 * (term - 26) + td]] <- term
-    extracted_data$member[[20 * (page - 1) + 9 * (term - 26) + td]] <-
+    extracted_data$term[[(180 * (term - 26)) + (20 * (page - 1)) + td]] <- term
+    
+    extracted_data$member[[(180 * (term - 26)) + (20 * (page - 1)) + td]] <-
       html_text(html_nodes(list_of_pages[[page + 9 * (term - 26)]],
                            css = "#constituency .c-member-list-item__name-content"))[[td]]
     }
